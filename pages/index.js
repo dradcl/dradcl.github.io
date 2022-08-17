6c01b6db2dd257344c04ae05cb9f68f083eb8c19
Dominick <53679096+karmakittenx@users.noreply.github.com>
2022-08-16 20:38:59 -0400

    Update meta tags and mobile view

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import Grid from '@mui/material/Grid';
 import Projects from '../src/Projects'
 import Fade from '@mui/material/Fade';
 import { useState } from "react";
+import Head from 'next/head';
 
 const themeDark = createTheme({
   palette: {
@@ -26,6 +27,15 @@ function Portfolio() {
     
     return (
       <Container>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta property="og:title" content="Dominick's Portfolio" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://dradcliffe.me/" />
+          <meta property="og:image" content="https://media-exp2.licdn.com/dms/image/C4D03AQEVderMYSXbww/profile-displayphoto-shrink_800_800/0/1644619419517?e=1662595200&v=beta&t=8UCbqDtxI55m7s9hJtjyAdke96M0O4hjNGEU_8bYqyk" />
+          <meta property="og:description" content="About me, my projects, and experience." />
+          <meta name="theme-color" content="#00FFF5" />
+        </Head>
         <GlobalStyles
           styles= {{
             body: { backgroundColor: "#222831" }
